d4e5c4b85480cd9b2a718dbdee49f056b60236eb
Swa11ace7 <159957716+Swa11ace7@users.noreply.github.com>
2025-06-29 16:23:03 -0700

    Update Restore-SQL.ps1

--- Restore-SQL.ps1
+++ Restore-SQL.ps1
@@ -13,6 +13,32 @@ $sqlServerInstanceName = "SRV19-PRIMARY\SQLEXPRESS"
 #Sets the database name with this variable
 $databaseName = "ClientDB"
 
+
+# Create Connection to Server
+$connectionString = "Data Source=$($sqlServerInstanceName);Initial Catalog=master;Inegrated Security=True;"
+$sqlConnection = New-Object System.Data.SqlClient.SqlConnection
+$sqlConnection.ConnectionString = $connectionString
+$sqlConnection.Open()
+
+# Create query to detect if database alreadt exists
+$detectQuery = "SELECT name FROM sys.databases WHERE name = '$($databaseName)'"
+
+# Create Command object to run query 
+$sqlCommand = New-Object System.Data.SqlClient.SqlCommand $detectQuery, $sqlConnection
+$sqlReader = $sqlCommand.ExecuteReader()
+
+if($sqlReader.Read())
+{
+    $sqlReader.Close()
+    Write-Host -ForegroundColor Cyan "[SQL]: $($databaseName) Database Found. Deleting"
+    # Create query and command to dorp database
+    $dropQuery = "ALTER DATABASE [$($databaseName)] SET SINGLE_USER WITH ROLLBACK IMMEDIATE DROP DATABASE [$($databaseName)]"
+    $sqlCommand.CommandText = $dropQuery
+    $sqlCommand.ExecuteNonQuery() | Out-Null
+}
+$sqlReader.Close()
+$sqlReader.Dispose()
+
 $sqlServerObject = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Server -ArgumentList $sqlServerInstanceName
 
 $databaseObject = Get-SqlDAtabase -ServerInstance $sqlServerInstanceName -Name $databaseName -ErrorAction SilentlyContinue
@@ -29,14 +55,14 @@ if ($databaseObject) {
     $databaseObject.drop()
 }
 else {
-    White-Host "[SQL]: $($databaseName) database not found"
+    Write-Host "[SQL]: $($databaseName) database not found"
 }
 
 
 # Creates a new database
 $databaseObject = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Database -ArgumentList $sqlServerObject, $databaseName
 $databaseObject.Create()
-White-Host -ForegroundColor Cyan "[SQL]: Database Created:[$($sqlServerInstanceName)].[$($databaseName)]"
+Write-Host -ForegroundColor Cyan "[SQL]: Database Created:[$($sqlServerInstanceName)].[$($databaseName)]"
 
 
 #Creates a new database table
@@ -48,11 +74,11 @@ Write-Host -ForegroundColor Cyan "[SQL]: Table Created:[$($sqlServerInstanceName
 
 # Inserts new data into the table
 $InsertQuery = "INSERT INTO [$($schema)].[$($tableName)] (FirstName, LastName, DisplayName, PostalCode, OfficePhone, MobilePhone)"
-$NewClient = Import-Csv -Path $PSScriptRoot\NewClientData.csv
+$NewClients = Import-Csv $PSScriptRoot\NewClientData.csv
 
 Write-Host -ForegroundColor Cyan "[SQL]: Inserting Data"
-foreach ($NewClient in $NewClient) {
-    $Values = " VALUES ('$(NewClient.FirstName)',
+foreach ($NewClient in $NewClients) {
+    $Values = " VALUES ('$($NewClient.FirstName)',
      '$($NewClient.LastName)', 
      '$($NewClient.DisplayName)', 
      '$($NewClient.PostalCode)', 
@@ -60,5 +86,21 @@ foreach ($NewClient in $NewClient) {
      '$($NewClient.MobilePhone)')"
      $query = $InsertQuery + $Values
      Invoke-Sqlcmd -Database $databaseName -ServerInstance $sqlServerInstanceName -Query $query
+
 }
+
+
+# Read Data
+Write-Host -ForegroundColor Cyan "[SQL]: Reading Data"
+$selectQuery = "SELCT * FROM $($tableName)"
+$Clients = Invoke-Sqlcmd -Database $databaseName -ServerInstance $sqlServerInstanceName -Query $query
+foreach ($Client in $Clients)  {
+    Write-Host "Client Name: $($Client.FirstName) $($Client.LastName)"
+    Write-Host "Display Name: $($Client.DisplayName)"
+    Write-Host "Postal Code: $($Client.PostalCode)"
+    Write-Host "Office Phone: $($Client.OfficePhone)"
+    Write-Host "Mobile Phone: $($Client.MobilePhone)"
+    Write-Host "-------------"
+}
+Write-Host -ForegroundColor Cyan "[SQL]: SQL Tasks Complete"
 Invoke-Sqlcmd -Database ClientDB -ServerInstance .\SQLEXPRESS -Query 'SELECT * FROM dbo.Client_A_Contacts' > .\SqlResults.txt
