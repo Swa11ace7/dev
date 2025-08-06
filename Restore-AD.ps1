29c447aca550d407b7a58b30eaa984998f27b73d
Swa11ace7 <159957716+Swa11ace7@users.noreply.github.com>
2025-08-06 16:25:22 -0700

    Update Restore-AD.ps1

--- Restore-AD.ps1
+++ Restore-AD.ps1
@@ -1,83 +1,52 @@
-<# Author : Shane Wallace 
-# Student Id: 010895068
-#>
+# path of the new OU to add
 
-$domain = Get-ADDomain
-# Creating a New User Account
-$newUser = @{
-    SamAccountName = "mcribb"
-    Name = "Mark Cribb"
-    GivenName = "Mark"
-    Surname = "Cribb"
-    DisplayName = "Mark Cribb"
-    UserPrincipalName = "mrcibb@consultingfirm.com"
-    Path = $domain.UserContainer
-    AccountPassword = (ConvertTo-SecureString "P@ssword12345!" -AsPlainText -Force)
-    Enabled = $true
-}
-New-ADUser @newUser
+$newOU = "OU=Finance,DC=consultingfirm,DC=com"
 
-# Resetting a User Password
-$user = "mcribb"
-$newPassword = (ConvertTo-SecureString "N3WP@ssword12345!" -AsPlainText -Force)
-Set-ADAccountPassword -Identity $user -NewPassword $newPassword -Reset
 
-# Unlocking a User Account
-Unlock-ADAccount -Identity $user
+# check if AD OU exists, delete and recreate if so
 
-# Adding a User Account
-$group = "CN=FinanceGroup, $($domain.DistinguishedName)"
-Add-ADGroupMember -Identity $group -Members $user
+if ([adsi]::Exists("LDAP://$newOU")) {
 
-# Checks for the existence of the OU named "Finance" and removes it if found
-$AdRoot = $domain.DistinguishedName
-$DnsRoot = $domain.DnsRoot
-$OUCanonicalName = "Finance"
-$OUDisplayName = "Finance"
-$AdPath = "OU=$($OUCanonicalName),$($AdRoot)"
+Write-Host "$newOU already exists. Deleting and recreating"
 
-try {
-    $OU =  Get-ADOrganizationalUnit -Identity $AdPath
-    Write-Host "OU not found"
-}
-catch {
-    $OU = $null
+Remove-ADOrganizationalUnit -Identity $newOU -Recursive -confirm:$false
+
 }
 
-If ($OU) {
-    Set-ADOrganizationUnit -Identity $AdPath -ProtectedFromAccidentalDeletion:$false
-    Remove-ADOrganizationalUnit -Identity $AdPath -Recursive -Confirm:$false
-    Write-Host "OU found "
-    Write-Host "OU removed"
+
+New-ADOrganizationalUnit -Name Finance -ProtectedFromAccidentalDeletion $False
+
+Write-Host "created $newOU"
+
+
+# use $PSScriptRoot instead of absolute (full) path to file so that the script will work on other systems and locations
+
+$NewAD = Import-CSV $PSScriptRoot\financePersonnel.csv
+
+
+foreach ($ADuser in $NewAD)
+
+{
+
+
+$First = $ADUser.First_Name
+
+$Last = $ADUser.Last_Name
+
+$Name = $First + " " + $Last
+
+$SamName = $ADUser.samAccount
+
+$Postal = $ADUser.PostalCode
+
+$Office = $ADUser.OfficePhone
+
+$Mobile = $ADUser.MobilePhone
+
+
+New-AdUser -GivenName $First -Surname $Last -Name $Name -SamAccountName $SamName -DisplayName $Name -PostalCode $Postal -MobilePhone $Mobile -OfficePhone $Office -Path $newOU
+
 }
 
-New-ADOrganizationalUnit -Path $AdRoot -Name $OUCanonicalName -DisplayName $OUDisplayName 
 
-# Searching for Inactive User Accounts
-$dataFilter = (Get-Date).AddDays(-90)
-$inactiveUsers = Get-ADUser -Filter {LastLogonDate -lt $dataFilter} -Properties LastLogonDate
-
-$inactiveUsers | Select-Object Name, LastLogonDate
-
-
-# Import the CSV file into the OU
-$NewADUsers = Import-Csv "c:\Source\Requirements2\financePersonnel.csv"
-
-
-ForEach ($ADUser in $NewADUsers) {
-    $Attributes = @{
-        GivenName           = $($ADUser.First_Name)
-        Surname             = $($ADUser.Last_Name)
-        Name                = "$($ADUser.First_Name) $($ADUser.Last_Name)"
-        DisplayName         = "$($ADUser.First_Name) $($ADUser.Last_Name)"
-        SamAccountName      = $($ADUser.samAccountName)
-        UserPrincipalName   = "$($ADUser.samAccountName)@$($DnsRoot)"
-        PostalCode          = $($ADUser.PostalCode)
-        OfficePhone         = $($ADUser.OfficePhone)
-        MobilePhone         = $($ADUser.MobilePhone)
-        Path                = $AdPath
-    }
-    New-ADUser @Attributes
-}
-# Used to create a ADResults file
-Get-ADUser -Filter * -SearchBase "ou=Finance,dc=consultingfirm,dc=com" -Properties DisplayName,PostalCode,OfficePhone,MobilePhone > .\AdResults.txt
+Get-ADUser -Filter * -SearchBase “ou=Finance,dc=consultingfirm,dc=com” -Properties DisplayName,PostalCode,OfficePhone,MobilePhone > $psscriptroot\AdResults.txt
